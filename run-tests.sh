7923e75864f74d235f3f060eaa372f1a82b38e2c
Stephen Cox <stephen-cox@users.noreply.github.com>
2020-06-14 11:26:21 +0100

    Run JS tests (#2)
    * Added chromedriver to docker compose. #1
    
    * Updated GitHub actions to use run tests script. #1
    
    * Fixed error in GitHub actions script. #1
    
    Co-authored-by: Stephen Cox <stephen@agile.coop>

--- run-tests.sh
+++ run-tests.sh
@@ -2,20 +2,35 @@
 
 ##
 # Run all tests!
+RESULT=0
 
-# Ensure everything is up to date
+# Fix ownership of code base.
+docker exec -t drupal bash -c "chown -R docker:docker /var/www/html"
+
+# Ensure everything is up to date.
 docker exec -u docker -t drupal bash -c "cd /var/www/html && composer install"
 
-# Coding standards
+# Coding standards checks.
 echo "Checking coding standards"
 docker exec -t drupal bash -c "cd /var/www/html && ./bin/phpcs"
+if [ $? -ne 0 ]; then
+  ((RESULT++))
+fi
 
-# Deprecated code
+# Deprecated code checks.
 echo "Checking for deprecated code"
 docker exec -t drupal bash -c "cd /var/www/html && ./bin/phpstan analyse -c ./phpstan.neon ./web/profiles/contrib/localgov/ ./web/modules/contrib/localgov_*"
+if [ $? -ne 0 ]; then
+  ((RESULT++))
+fi
 
-# Tests
+# PHPUnit tests.
 echo "Running tests"
 docker exec -t drupal bash -c "mkdir -p /var/www/html/web/sites/simpletest && chmod 777 /var/www/html/web/sites/simpletest"
 docker exec -u docker -t drupal bash -c "cd /var/www/html && ./bin/phpunit --testdox"
+if [ $? -ne 0 ]; then
+  ((RESULT++))
+fi
 
+# Set return code depending on number of tests that failed.
+exit $RESULT
