cec465d3c690292e0b9ea3fcb2f8d8be02c65e7b
Stephen Cox <stephen@agile.coop>
2020-11-05 15:32:53 +0000

    Use Composer 2 in environment when running tests rather than in Docker

--- run-tests.sh
+++ run-tests.sh
@@ -29,7 +29,7 @@ echo "Running tests"
 docker exec -t drupal bash -c "mkdir -p /var/www/html/web/sites/simpletest && chmod 777 /var/www/html/web/sites/simpletest"
 # Update PHPUnit's env var declarations; Paratest does not pass these to PHPUnit :(
 docker exec -u docker -t drupal bash -c 'sed -i "s#http://localgov.lndo.site#http://drupal#; s#mysql://database:database@database/database#sqlite://localhost/dev/shm/test.sqlite#" /var/www/html/phpunit.xml.dist'
-docker exec -u docker -t drupal bash -c "cd /var/www/html && ./bin/paratest --processes=4"
+docker exec -u docker -t drupal bash -c "cd /var/www/html && ./bin/paratest --processes=4 --verbose=1"
 if [ $? -ne 0 ]; then
   ((RESULT++))
 fi
