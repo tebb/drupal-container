947a52568105e8703c5188b64b0dfce192b73c7e
Stephen Cox <stephen@agile.coop>
2020-11-05 14:53:17 +0000

    Use SQLite database in memory (/dev/shm) when running tests

--- run-tests.sh
+++ run-tests.sh
@@ -28,8 +28,8 @@ fi
 echo "Running tests"
 docker exec -t drupal bash -c "mkdir -p /var/www/html/web/sites/simpletest && chmod 777 /var/www/html/web/sites/simpletest"
 # Update PHPUnit's env var declarations; Paratest does not pass these to PHPUnit :(
-docker exec -u docker -t drupal bash -c 'sed -i "s#http://localgov.lndo.site#http://drupal#; s#mysql://database:database@database/database#sqlite://localhost//tmp/test.sqlite#" /var/www/html/phpunit.xml.dist'
-docker exec -u docker -t drupal bash -c "cd /var/www/html && ./bin/paratest --processes=4 --verbose=1"
+docker exec -u docker -t drupal bash -c 'sed -i "s#http://localgov.lndo.site#http://drupal#; s#mysql://database:database@database/database#sqlite://localhost/dev/shm/test.sqlite#" /var/www/html/phpunit.xml.dist'
+docker exec -u docker -t drupal bash -c "cd /var/www/html && ./bin/paratest --processes=4"
 if [ $? -ne 0 ]; then
   ((RESULT++))
 fi
